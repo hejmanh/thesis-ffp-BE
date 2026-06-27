f8961f65552e9c94788c7b5fa00e5a8466bf3641
Linh Dang Phan Khanh <khanhlinhdang2301@gmail.com>
2026-06-27 17:32:33 +0700

    Update survey question

---
+++ db/migrations/016_attention_check_question.sql
@@ -0,0 +1,12 @@
+INSERT INTO survey_question (
+    id,
+    code,
+    category,
+    question_text,
+    question_text_vi
+)
+VALUES (
+    18,'RQ1','RQ',
+    'To help us ensure response quality, please select "Agree" for this statement.',
+    'Để giúp chúng tôi đảm bảo chất lượng phản hồi, vui lòng chọn "Đồng ý" cho tuyên bố này.'
+);
